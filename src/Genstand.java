91597dd774319763314e63356753295577783354
sune <sune1309@edu.easj.dk>
2019-10-08 14:47:41 +0200

    Initial commit

---
+++ src/Genstand.java
@@ -0,0 +1,5 @@
+public class Genstand {
+    public void setName() {
+
+    }
+}
---
+++ src/SpilleFelt.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class SpilleFelt {
+    public String beskrivelse;
+    public int bredde;
+    public int laengde;
+    public Queue<Genstand> genstande;
+    public String lokationsType;
+
+    public String beskriv() {
+        return beskrivelse;
+    }
+
+    public int hvorStortiKvm() {
+        return 100;
+    }
+
+    public String hvorStort() {
+        int areal = bredde * laengde;
+        if (areal>10) {return  "lille";}
+
+        if (areal<11) {return "mellem"; }
+
+        if (areal<101) {return "stort";}
+
+
+
+
+return "lille";
+
+
+
+    }
+
+
+
+    public String listGenstande() {
+        return null;
+    }
+
+    public void setLokationsType(String ørken) {
+
+    }
+
+    public void setBeskrivelse(String s) {
+
+    }
+
+    public void setBredde(int i) {
+
+    }
+
+    public int getBredde() {
+        return 0;
+    }
+
+    public String getLokationsType() {
+        return null;
+    }
+
+    public String getBeskrivelse() {
+        return null;
+    }
+    public String setName() {
+        return null;
+    }
+    public String getName() {
+        return null;
+    }
+
+    public void setLaengde(int i) {
+
+    }
+
+    public void setGenstande(ArrayList<Genstand> genstande) {
+
+    }
+
+    public List<Object> getGenstande() {
+        return null;
+    }
+}
